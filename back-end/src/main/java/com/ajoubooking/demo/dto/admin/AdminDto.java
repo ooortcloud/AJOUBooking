b241cb3afe8a6f76c7eed3bfdf4909b0c442e583
ooortcloud <110080440+ooortcloud@users.noreply.github.com>
2023-12-01 16:55:20 +0900

    1201 login drift update

--- back-end/src/main/java/com/ajoubooking/demo/dto/admin/AdminDto.java
+++ back-end/src/main/java/com/ajoubooking/demo/dto/admin/AdminDto.java
@@ -10,7 +10,5 @@ import lombok.Setter;
 @Setter  // @ModelAttribute를 쓰기 위해서는 @Setter가 필요함
 public class AdminDto {
     @NotBlank(message = "공백은 입력할 수 없습니다.")  // 필드값에 대해 공백 또는 빈칸만으로 데이터가 들어오지 못하도록 설정(Bean Validation)
-    private String adminId;
-    @NotBlank(message = "공백은 입력할 수 없습니다.")
     private String pw;
 }
