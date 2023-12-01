19761e07e564d5706d3191f51a91728574cc8b98
ooortcloud <110080440+ooortcloud@users.noreply.github.com>
2023-12-01 18:18:07 +0900

    1201 error message complete

--- back-end/src/main/java/com/ajoubooking/demo/controller/AdminController.java
+++ back-end/src/main/java/com/ajoubooking/demo/controller/AdminController.java
@@ -18,26 +18,38 @@ public class AdminController {
     private AdminService adminService;
 
     @GetMapping("")
-    public String loginPage(@ModelAttribute AdminDto dto, Model model) {  // 객체에 맞게 요청 파라미터 값들을 자동으로 바인딩
-        model.addAttribute("dto", AdminDto.builder().build());  // 빈 객체를 넣어서 th:object에 전달해줘야 함
+    public String loginPage(Model model) {
+        model.addAttribute("dto", AdminDto.builder().build());  // th:object에 정의한 빈 객체를 넣어 전달해줘야 함
         return "newLoginPage";
     }
 
+    /**
+     * @ModelAttribute의 디폴트 규칙은 도메인명과 동일하게 필드 이름을 갖는 것. 그 이름을 쓰기 싫으면 직접 이름을 지정해줘야 한다.
+     * 참고) @ModelAttribute는 HTTP 요청 파라미터를 처리할 때 사용한다. URL 쿼리 스트링, POST form을 처리하는 것이 대표적.
+     *
+     * BindingResult 인터페이스를 받아서 Bean Validation 적용
+     *
+     * @return 오류 뜨면 오류 메시지를 출력, 로그인 성공하면 세션을 생성하며 메인 페이지로 이동시킴.
+     */
     @PostMapping("")
-    public String validation(@Valid @ModelAttribute("dto") AdminDto dto, BindingResult bindingResult) {  // Bean Validation 적용
-        // 아예 입력 형식이 잘못된 경우 돌려보냄. 에러 메세지는 처리된 예외에 맞춰 자동으로 배정됨.
+    public String validation(@Valid @ModelAttribute("dto") AdminDto dto, BindingResult bindingResult) {
+
+        // AdminDto에 걸어둔 유효성 검사에 위반되면, bindingResult 객체에 dto에서 설정해둔 메세지를 담아서 리턴.
         if(bindingResult.hasErrors()){
-            System.out.println("글로벌 오류");
-            ObjectError badInputError = new ObjectError("globalError", "PW 입력이 잘못되었습니다.");
-            return "redirect:/login";
+            return "newLoginPage";
         }
 
         // 계정 조회에 실패한 경우 돌려보냄
         boolean b = adminService.validateRequestLogin(dto.getPw());
         if(!b){
             System.out.println("계정 조회 오류");
-            bindingResult.reject("NotBlank.login");  // 에러 메세지 직접 지정
-            return "redirect:/login";
+            bindingResult.rejectValue("pw", null, "pw 입력이 잘못되었습니다.");
+            /**
+             * bindingResult.rejectValue()  << 필드 에러
+             * bindingResult.reject()  << 객체 에러
+             * errorCode : 'errors.properties'에 정의한 매크로 메세지를 읽어옴. null인 경우 설정된 defaultMessage를 넘김.
+             */
+            return "newLoginPage";
         }
 
         return "redirect:/login/main";  // 리다이랙트로 새 페이지를 띄우지 않고 현재 페이지에서 곧바로 이동시킴. 유효성 검사 필요
--- back-end/src/main/java/com/ajoubooking/demo/dto/admin/AdminDto.java
+++ back-end/src/main/java/com/ajoubooking/demo/dto/admin/AdminDto.java
@@ -9,6 +9,7 @@ import lombok.Setter;
 @Getter 
 @Setter  // @ModelAttribute를 쓰기 위해서는 @Setter가 필요함
 public class AdminDto {
+    // 필터링을 dto 단에서 처리하기 때문에, 도메인 단에서는 필터링할 필요가 없어짐!
     @NotBlank(message = "공백은 입력할 수 없습니다.")  // 필드값에 대해 공백 또는 빈칸만으로 데이터가 들어오지 못하도록 설정(Bean Validation)
     private String pw;
 }
