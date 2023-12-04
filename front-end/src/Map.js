6df816a64ab59a555245a5a83743fbdd410322d7
tokemoo <jgk0362@gmail.com>
2023-12-04 17:50:31 +0900

    1204_v1

--- front-end/src/Map.js
+++ front-end/src/Map.js
@@ -1,24 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import $ from 'jquery';
-import 'magnific-popup/dist/magnific-popup.css'; // Magnific Popup의 CSS 파일을 추가
+import 'magnific-popup/dist/magnific-popup.css';
 import './Map.css';
 
 const Map = ({ mapData }) => {
-  useEffect(() => {
-    const $ = window.jQuery;
-
-    if ($) {
-      // Magnific Popup 초기화
-      $('.popup-link').magnificPopup({
-        type: 'image',
-        gallery: {
-          enabled: true,
+  const canvasRef = useRef(null);
+  const initializeMagnificPopup = () => {
+    // jQuery 대신 $ 사용
+    if (window.jQuery) {
+      window.jQuery.magnificPopup.open({
+        items: {
+          src: '/map.png',
+          type: 'image',
         },
-        // 추가: 팝업에 Close 버튼 추가
         closeBtnInside: false,
-        // 추가: 팝업 이미지 화면 중앙에 고정
         mainClass: 'mfp-fade mfp-align-center-horizontally mfp-align-center-vertically',
-        // 추가: 팝업 이미지가 나올 때 팝업 이미지를 제외한 뒷배경을 어둡게
         closeOnBgClick: false,
         closeOnContentClick: false,
         closeOnEsc: false,
@@ -32,34 +28,116 @@ const Map = ({ mapData }) => {
         },
         callbacks: {
           beforeOpen: () => {
-            // 팝업 이미지를 제외한 뒷배경을 어둡게
             $('body').css('background-color', 'rgba(0, 0, 0, 0.5)');
           },
           afterClose: () => {
-            // 뒷배경을 원래대로
             $('body').css('background-color', 'white');
+            $('body').css('overflow', 'auto');
           },
         },
       });
     } else {
       console.error('jQuery is not available.');
     }
-  }, []);
+  };
+  useEffect(() => {
+    
+
+    const drawRedDotOnCanvas = (img, coordinates) => {
+      const canvas = canvasRef.current;
+      const context = canvas.getContext('2d');
+
+      context.clearRect(0, 0, canvas.width, canvas.height);
+
+      if (img.width && img.height) {
+        const ratioX = canvas.width / img.width;
+        const ratioY = canvas.height / img.height;
+
+        context.fillStyle = 'red';
+        context.beginPath();
+        context.arc(coordinates.x * ratioX, coordinates.y * ratioY, 5, 0, 2 * Math.PI);
+        context.fill();
+
+        const x = coordinates.x * ratioX;
+        const y = coordinates.y * ratioY;
+
+        context.fillStyle = 'black';
+        context.fillText(`(x: ${x}, y: ${y})`, x, y);
+      }
+    };
+
+    if (mapData) {
+      const img = new Image();
+      img.src = '/map.png';
+
+      img.onload = () => {
+        const canvas = canvasRef.current;
+        canvas.width = img.width;
+        canvas.height = img.height;
+
+        console.log('Image Width:', img.width, 'Image Height:', img.height);
+
+        const coordinates = getBookshelfCoordinates(mapData.bookshelfNum, mapData.columnNum, img.width, img.height);
+
+        console.log('Computed Coordinates:', coordinates);
+
+        drawRedDotOnCanvas(img, coordinates);
+        initializeMagnificPopup(); // 이미지가 로드된 후 팝업 열기
+      };
+    }
+  }, [mapData]);
+
+  const getBookshelfCoordinates = (bookshelfNum, columnNum, imageWidth, imageHeight) => {
+    const bookshelfStartX = 1168 + (bookshelfNum - 1) * 64.5;
+    const bookshelfStartY = 45;
+    const bookshelfEndY = 165;
+
+    if (columnNum <= 6) {
+      const x = bookshelfStartX;
+      const y = bookshelfStartY + (columnNum - 1) * 24;
+      return { x, y };
+    }
+
+    const x = bookshelfStartX + 21;
+    const y = bookshelfEndY - (columnNum - 7) * 24;
+    return { x, y };
+  };
 
   return (
-    <div>
-      <p>찾으시는 책의 위치는 지도에 표시되어 있습니다. </p>
-      {/* 여기에 이미지 파일 경로와 alt 텍스트를 넣어주세요 */}
-      <a href="/map.png" className="popup-link">
+    <div style={{ position: 'relative' }}>
+      {/* 팝업 열기 위한 커스텀 링크 */}
+      <div className="popup-link" onClick={() => initializeMagnificPopup()}>
         <img id="book-map" src="/map.png" alt="Map" />
-      </a>
+      </div>
+
+      {/* 버튼을 클릭하여 팝업 열기 */}
+      <button className="open-popup-button" onClick={() => initializeMagnificPopup()}>
+        Open Map
+      </button>
+
       {mapData && (
         <div>
-          <p>책 분야 : "{mapData.category}"</p>
-          <p>책장 번호 : "{mapData.bookshelfNum}"</p>
-          <p>칼럼 번호 : "{mapData.columnNum}"</p>
+          <p>책의 위치는 지도의 🔴 을 봐주세요!</p>
+          <p>
+            책 분야 : "{mapData.category === 0
+              ? '총류 및 컴퓨터'
+              : mapData.category === 1
+              ? '철학'
+              : mapData.category === 2
+              ? '종교'
+              : mapData.category === 3
+              ? '사회과학'
+              : mapData.category === 4
+              ? '언어'
+              : mapData.category === 5
+              ? '자연과학'
+              : '알 수 없는 분야'}"
+          </p>
+          <p>책장 번호 : "{mapData.bookshelfNum}번"</p>
+          <p>칼럼 번호 : "{mapData.columnNum}구간"</p>
         </div>
       )}
+      <canvas ref={canvasRef} style={{ position: 'absolute', top: 0, left: 0 }}></canvas>
     </div>
   );
 };
